134ae904d4a60b3c6971fd316277539a34f90ace
YogendraPJoshi <yogendraJoshi02@gmail.com>
2023-07-27 09:46:08 +0530

    [Yogendra]Committing my1st code

---
+++ src/test/java/eCommerceProject/Amazon.java
@@ -0,0 +1,20 @@
+package eCommerceProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Amazon {
+  @Test
+  public void azmazonTest() throws InterruptedException 
+  {
+	  
+	  WebDriver driver= new ChromeDriver();
+	  Reporter.log("opening amzon", true);
+	  driver.get("https://www.amazon.in/");
+	  Thread.sleep(7000);
+	  driver.close();
+	  
+  }
+}
---
+++ src/test/java/eCommerceProject/test.java
@@ -0,0 +1,16 @@
+package eCommerceProject;
+
+public class test
+{
+	
+	public static void main(String[] args) {
+		String ar[]= {"hi","hello"};
+		
+		for(String a:ar)
+		{
+			System.out.println(a);
+		}
+		
+	}
+
+}
